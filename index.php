c50d0c98b72238b9e75889d5c41500ea2d470bf0
AlejandroYam <alejandro98b182@gmail.com>
2021-05-24 08:42:57 -0500

    nuevo commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('masterpage/header.php')?>
+
+<body>
+
+<?php include('masterpage/menu.php')?>
+
+<?php include('page/home.php')?>
+
+<?php include('masterpage/footer.php')?>
+
+    
+</body>
+
+</html>
